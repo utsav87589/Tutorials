5e9df0b7ae19cc5797e9ae1ed1e222a973bf6693
utsav87589 <gkundukundu987@gmail.com>
2026-03-19 19:21:55 -0400

    worked with the forms and other routes

--- Flask/sample_project/app.py
+++ Flask/sample_project/app.py
@@ -1,17 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home() : 
 
-    return 'Switching to the flask mid project lol!'
+    return render_template('index.html')
 
 
 @app.route('/about')
 def about() :
 
-    return 'This is about page though'
+    return 'This is about page'
+
+@app.route('/submit', methods = ['GET', 'POST'])
+def submit() : 
+    name = None
+    error_message = None
+
+    if request.method == 'POST' :
+        name = request.form.get('username', '').strip()
+        # print(f"user entered the name : {name}")
+
+        if not name : 
+            error_message = 'You did not entered any name!'
+            name = None
+
+    return render_template('submit.html', name = name, error = error_message)
+
 
 if __name__ == '__main__' : 
-    app.run()
+    app.run(debug = True)
