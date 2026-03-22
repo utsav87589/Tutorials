b28658fdd110a8ec7eb9e25de040770680d228c3
utsav87589 <gkundukundu987@gmail.com>
2026-03-21 20:56:11 -0400

    updated app.py as per new things

--- Flask/login_app/app.py
+++ Flask/login_app/app.py
@@ -1,19 +1,51 @@
 from flask import request, Response, url_for, session, redirect, Flask, render_template
 
 app = Flask(__name__)
-app.secretkey = 'secret'
+app.secret_key = 'secret'
 
 
-### main home page (aka login page) directory
+### main home page (login page) directory
 @app.route('/', methods = ['GET', 'POST'])
 def login() : 
 
-    username = request.form.get('username')
-    password = request.form.get('password')
+    if request.method == 'POST' : 
 
-    print(f"{username} :: {password}")
+        ### getting the username and password from the form
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        ### printing into the terminal to make sure that it's working
+        print(f"{username} :: {password}")
+
+        valid_users = {
+            'utsav' : '123',
+            'bob' : '123',
+            'joe' : 'joe123'
+        }
+
+        if username in valid_users.keys() and password == valid_users[username]: 
+            session['user'] = username ### store into the session
+            return redirect(url_for('welcome'))
+
+        else : 
+            return render_template('login.html', error = 'Invalid credentials')
 
     return render_template('login.html')
 
-if __name__ == '__main__' : 
-    app.run(debug = True)
+### welcome page (after successful login)
+@app.route('/welcome')
+def welcome() : 
+
+    if 'user' in session :
+        return render_template('welcome.html', user = session['user'])
+
+    else : 
+        return redirect(url_for('login'))
+    
+
+### logout page
+@app.route('/logout')
+def logout() : 
+
+    session.pop('user', None)
+    return redirect(url_for('login'))
